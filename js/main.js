770355196a7032ce2720b1815b669be25cb29824
Lucas Dupin <lucas@commit.to>
2013-05-14 15:31:58 -0300

    new alignments, menu, asco page

--- js/main.js
+++ js/main.js
@@ -1,27 +1,63 @@
 window.onload = function(){
+
+	/**************
+	Menu navigation
+	***************/
+
+	// About section
+	document.getElementsByClassName('about')[0].onclick = function(){
+		document.getElementById('about').style.display = 'block'
+	}
+	document.getElementsByClassName('close')[0].onclick = function(){
+		document.getElementById('about').style.display = 'none';
+	}
+
+	// List of projects
+	PROJECTS = []
+	// Get them from the menu
+	var p = document.getElementsByClassName('projects')[0].getElementsByTagName('a')
+	var summaries = document.getElementsByClassName('projects')[0].getElementsByTagName('summary')
+	for (var i = 0; i < p.length; i++) {
+		PROJECTS.push(p[i].href);
+		// Apply rotation
+		transform(p[i], 'rotate(' + 360/p.length*i + 'deg)');
+		
+		// Flip
+		if (i > p.length/4 && i < p.length/4*3){
+			transform(p[i].getElementsByTagName('span')[0], 'rotate(180deg)');
+			
+			rotateSpan = document.createElement('span')
+			rotateSpan.innerHTML = summaries[i].innerHTML
+			transform(rotateSpan, 'rotate(180deg)');
+			
+			summaries[i].innerHTML = ""
+			summaries[i].appendChild(rotateSpan)
+			transform(summaries[i], 'rotate(' + (-360/p.length*i + 180) + 'deg)');
+		} else {
+			transform(summaries[i], 'rotate(' + -360/p.length*i + 'deg)');
+		}
+		// auto close on click
+		p[i].onclick = function(){
+			document.getElementById('about').style.display = 'none';
+		}
+	};
+
+	/**************************
+	Random project button
+	**************************/
 	var currentProject = 0;
+	// Random project click
 	document.getElementById('next').onclick = function(evt){
 		evt.preventDefault()
-		document.getElementById('content').src = PROJECTS[currentProject++];
+		document.getElementById('content').src = PROJECTS[currentProject++%PROJECTS.length];
 	}
+	// Randomize first
 	document.getElementById('content').src = PROJECTS[currentProject++];
 }
 
-var shuffle = function(array) {
-	var i, j, tmp, _i, _ref;
-	for (i = _i = 0, _ref = array.length; _i < _ref; i = _i += 1) {
-		j = Math.floor(Math.random() * (i + 1));
-		tmp = array[i];
-		array[i] = array[j];
-		array[j] = tmp;
-	}
-	return array;
-};
-
-
-PROJECTS = shuffle([
-		"http://cuco.cc/projects/obeijo",
-		"http://cuco.cc/projects/bailarinainfinita",
-		"http://cuco.cc/projects/slot",
-		"http://diarioaleatorio.co/"
-])
+function transform(el, val){
+	el.style['-moz-transform'] =
+	el.style['-webkit-transform'] =
+	el.style['transform'] =
+		val;
+}
